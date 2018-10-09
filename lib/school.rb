48263ca4d7b1243d783be568c967c414b549f69e
aaaaus <ausluft@gmail.com>
2018-10-09 18:05:53 -0400

    exercise completed

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+require "pry"
+
+class School
+
+def roster
+  @roster
+end
+
+def initialize(name)
+  @school = name
+  @roster = {}
+end
+
+def add_student(student_name, grade)
+  @roster[grade] = [] unless @roster[grade]
+  @roster[grade] << student_name
+end
+
+def grade(grade)
+  @roster[grade]
+end
+
+def sort
+  @roster.each do |grade, student_array|
+    @roster[grade] = student_array.sort
+  end
+end
+
+end
